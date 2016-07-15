95a04cf36a2bc735e69057ff84279b49a49f4f28
Sebastian <solais@solais.de>
2016-07-16 01:57:59 +0200

    2016-07-16_1468627078

--- main.cpp
+++ main.cpp
@@ -750,7 +750,7 @@ void compareSubsumption ()
 {
     while (true)
     {
-        NFA* A = TVAutomataGen(10, 10, 0.8, 0.8).generate();
+        NFA* A = TVAutomataGen(10, 3, 0.8, 0.8).generate();
         GameGrammar* G = TVGrammarGen(A->Sigma, 10, 10, 0.75, 0.85, 0.85, 0.85).generate();
 
         auto start = chrono::steady_clock::now();
